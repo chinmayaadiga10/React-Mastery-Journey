c6c97f94a57d1e3776b13050fd5a4ac370e8f5e0
Chinmaya Adiga <chinmayaadiga.sf@gmail.com>
2026-05-11 15:35:07 +0530

    feat : final weather app component

--- Weather App/src/WeatherApp.jsx
+++ Weather App/src/WeatherApp.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+import SearchBox from "./SearchBox";
+import InfoBox from "./InfoBox";
+
 const WeatherApp = () => {
+  const [weatherInfo, setWeatherInfo] = useState({
+    city: "Delhi",
+    feelslike: 24.84,
+    temp: 25.25,
+    tempMin: 25.05,
+    tempMax: 25.99,
+    humidity: 47,
+    weather: "haze",
+  });
+
+  const updateInfo = (newInfo) => {
+    setWeatherInfo(newInfo);
+  };
   return (
     <div>
-      <h2>The Weather App component</h2>
+      <SearchBox updateInfo={updateInfo} />
+      <InfoBox info={weatherInfo} />
     </div>
   );
 };
