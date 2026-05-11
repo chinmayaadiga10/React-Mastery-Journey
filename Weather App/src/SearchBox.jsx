0175edafd9a51f954880a1e50b98795a546efa0a
Chinmaya Adiga <chinmayaadiga.sf@gmail.com>
2026-05-11 15:37:59 +0530

    style: basic UI for component along with functions defined

--- Weather App/src/SearchBox.jsx
+++ Weather App/src/SearchBox.jsx
@@ -1,9 +1,26 @@
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 import "./SearchBox.css";
 
 const SearchBox = () => {
   return (
     <div>
-      <h3>Search Box Component</h3>
+      <h2>Search for the Weather</h2>
+      <div className="SearchBox">
+        <form onSubmit={handleSubmit}>
+          <TextField
+            id="city"
+            label="City name"
+            variant="outlined"
+            required
+            value={city}
+            onChange={changeCity}
+          />
+          <Button variant="contained" type="submit">
+            Search
+          </Button>
+        </form>
+      </div>
     </div>
   );
 };
