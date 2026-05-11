a65a94d3e575a0f8901c0274decf3085606c192f
Chinmaya Adiga <chinmayaadiga.sf@gmail.com>
2026-05-11 15:47:18 +0530

    fix : import css twice

--- Weather App/src/InfoBox.jsx
+++ Weather App/src/InfoBox.jsx
@@ -7,8 +7,6 @@ import ThunderstormIcon from "@mui/icons-material/Thunderstorm";
 import SunnyIcon from "@mui/icons-material/Sunny";
 import "./InfoBox.css";
 
-import "./InfoBox.css";
-
 const InfoBox = ({ info }) => {
   const HOT_URL =
     "https://images.unsplash.com/photo-1504370805625-d32c54b16100?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
