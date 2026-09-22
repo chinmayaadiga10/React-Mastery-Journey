909e272d3e4ec6643988241c6ae37bb478d50657
Chinmaya Adiga <chinmayaadiga.sf@gmail.com>
2026-09-22 23:58:20 +0530

    feat : reusable Input component

--- BMI Calculator App/src/InputField.jsx
+++ BMI Calculator App/src/InputField.jsx
@@ -1,5 +1,31 @@
-const InputField = () => {
-  return <div>InputField</div>;
+const InputField = ({
+  label,
+  id,
+  name,
+  type,
+  value,
+  onChange,
+  min,
+  required,
+}) => {
+  return (
+    <div>
+      <label htmlFor={id} className="font-medium text-lg tracking-wide">
+        {label}
+      </label>
+      <input
+        type={type}
+        id={id}
+        name={name}
+        value={value}
+        onChange={onChange}
+        min={min}
+        required={required}
+        className="ml-5 bg-white rounded-md pl-3 outline-0 text-blue-800 font-medium"
+      />
+      <br />
+    </div>
+  );
 };
 
 export default InputField;
