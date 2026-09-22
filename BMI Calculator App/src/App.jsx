68d17f03f9c595cb8243b31514a10d7ee24c1438
Chinmaya Adiga <chinmayaadiga.sf@gmail.com>
2026-09-23 00:02:21 +0530

    feat : render all components from App

--- BMI Calculator App/src/App.jsx
+++ BMI Calculator App/src/App.jsx
@@ -1,5 +1,12 @@
-const App = () => {
-  return <div>App</div>;
-};
+import BMICalculator from "./BMICalculator";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-linear-to-br from-slate-100 via-blue-100 to-indigo-300">
+      <BMICalculator />
+    </div>
+  );
+}
 
 export default App;
